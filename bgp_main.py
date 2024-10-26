c93691e6d3418f786e91d426acb6f233f56e4b56
elsa <els4lee@gmail.com>
2024-10-26 11:10:02 +0800

    Edit router initializing

--- bgp_main.py
+++ bgp_main.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python
-
 from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.log import lg, info, setLogLevel
-from mininet.util import dumpNodeConnections, quietRun, moveIntf
+from mininet.log import setLogLevel, info
+from mininet.util import dumpNodeConnections
 from mininet.cli import CLI
-from mininet.node import Switch, OVSKernelSwitch
+from mininet.node import OVSKernelSwitch
 
-from subprocess import Popen, PIPE, check_output
+from subprocess import Popen
 from time import sleep
 import os
 import termcolor as T
@@ -26,17 +24,13 @@ ROGUE_AS_NAME = 'S6'
 def log(s, col="green"):
     print(T.colored(s, col))
 
-class Router(Switch):
+class Router(OVSKernelSwitch):
     ID = 0
     def __init__(self, name, **kwargs):
         kwargs['inNamespace'] = True
-        Switch.__init__(self, name, **kwargs)
+        OVSKernelSwitch.__init__(self, name, **kwargs)
         Router.ID += 1
         self.switch_id = Router.ID
-
-    @staticmethod
-    def setup():
-        return
 
     def start(self, controllers):
         pass
@@ -44,27 +38,28 @@ class Router(Switch):
     def stop(self):
         self.deleteIntfs()
 
-    def log(self, s, col="magenta"):
-        print(T.colored(s, col))
+    @staticmethod
+    def setup():
+        os.system("sysctl -w net.ipv4.ip_forward=1")  # Enable IP forwarding on the host
 
 class Topology(Topo):  
     def __init__(self):
         super(Topology, self).__init__()
         num_hosts_per_as = 3
-        num_ases = 6 
+        num_ases = 6
         routers = []
 
         # Create routers
         for i in range(num_ases):
-            router = self.addSwitch('S%d' % (i+1))
+            router = self.addSwitch('S%d' % (i + 1), cls=Router)
             routers.append(router)
 
         # Create hosts and links within ASes
         for i in range(num_ases):
-            router = 'S%d' % (i+1)
+            router = 'S%d' % (i + 1)
             for j in range(num_hosts_per_as):
-                hostname = 'h%d-%d' % (i+1, j+1)
-                host = self.addHost(hostname, ip="10.%d.%d.1/24" % (i+1, j+1), defaultRoute="via 10.%d.%d.254" % (i+1, j+1))
+                hostname = 'h%d-%d' % (i + 1, j + 1)
+                host = self.addHost(hostname, ip="10.%d.%d.1/24" % (i + 1, j + 1), defaultRoute="via 10.%d.%d.254" % (i + 1, j + 1))
                 self.addLink(router, host)
 
         # Add links between ASes
@@ -79,24 +74,17 @@ class Topology(Topo):
 
         self.addLink('S1', 'S6')  # Link rogue AS to AS1
 
-def getIP(hostname):
-    AS, idx = hostname.replace('h', '').split('-')
-    AS = int(AS)
-    idx = int(idx) 
-    ip = '10.%d.%d.1/24' % (AS, idx) if AS < 6 else '20.0.%d.1/24' % idx
-    return ip
-
-def getGateway(hostname):
-    AS, idx = hostname.replace('h', '').split('-')
-    AS = int(AS)
-    idx = int(idx)  
-    gw = '10.%d.%d.254' % (AS, idx) if AS < 6 else '20.0.%d.254' % idx
-    return gw
-
-
-def startWebserver(net, hostname, text="Default web server"):
-    host = net.getNodeByName(hostname)
-    return host.popen("sudo python3 webserver.py --text '%s'" % text, shell=True)
+def start_router(router):
+    """Function to start zebra and bgpd for BGP configurations."""
+    zebra_cmd = f"/usr/lib/frr/zebra -f conf/zebra-{router.name}.conf -d -i /tmp/zebra-{router.name}.pid"
+    bgpd_cmd = f"/usr/lib/frr/bgpd -f conf/bgpd-{router.name}.conf -d -i /tmp/bgp-{router.name}.pid"
+    
+    router.cmd(zebra_cmd)
+    router.waitOutput()
+    router.cmd(bgpd_cmd)
+    router.waitOutput()
+    router.cmd("ifconfig lo up")  # Enable loopback interface
+    log(f"Started BGP and Zebra on {router.name}")
 
 def main():
     os.system("rm -f /tmp/S*.log /tmp/S*.pid logs/*")
@@ -104,15 +92,17 @@ def main():
     os.system("killall -9 zebra bgpd > /dev/null 2>&1")
     os.system('pgrep -f webserver.py | xargs kill -9')
 
-    net = Mininet(topo=Topology(), switch=Router)  
+    net = Mininet(topo=Topology(), switch=Router)
     net.start()
+
     for router in net.switches:
-        router.cmd("sudo sysctl -w net.ipv4.ip_forward=1")
+        router.cmd("sysctl -w net.ipv4.ip_forward=1")  # Enable IP forwarding per router
         router.waitOutput()
 
     log("Waiting %d seconds for sysctl changes to take effect..." % args.sleep)
     sleep(args.sleep)
 
+    # Start BGP and Zebra on each router
     for router in net.switches:
         if router.name == ROGUE_AS_NAME and not FLAGS_rogue_as:
             continue
@@ -129,12 +119,17 @@ def main():
         router.waitOutput()
         log("Starting zebra and bgpd on %s" % router.name)
 
+    # Configure hosts with IP and gateway
     for host in net.hosts:
-        log("Config host %s-eth0 %s, gateway: %s" % (host.name, getIP(host.name), getGateway(host.name)))
+        ip = "10.%d.%d.1/24" % (int(host.name.split('-')[0][1:]), int(host.name.split('-')[1]))
+        gw = "10.%d.%d.254" % (int(host.name.split('-')[0][1:]), int(host.name.split('-')[1]))
+        host.setIP(ip)
+        host.setDefaultRoute("via " + gw)
+        log(f"Configured host {host.name} with IP {ip} and gateway {gw}")
 
     log("Starting web servers", 'yellow')
-    startWebserver(net, 'h1-1', "Default web server")
-    startWebserver(net, 'h6-1', "*** Rogue web server ***")  # Rogue web server
+    net.getNodeByName('h1-1').popen("python3 -m http.server 80", shell=True)
+    net.getNodeByName('h6-1').popen("python3 -m http.server 80", shell=True)  # Rogue web server
 
     CLI(net)
     net.stop()
