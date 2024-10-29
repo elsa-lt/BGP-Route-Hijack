e3e80969ab660522bb42a53a38c482b0c4c3b895
elsa <els4lee@gmail.com>
2024-10-29 14:44:21 +0800

    conf test

--- bgp_main.py
+++ bgp_main.py
@@ -1,14 +1,21 @@
+#!/usr/bin/env python3
+
 from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.log import setLogLevel, info
-from mininet.util import dumpNodeConnections
+from mininet.log import lg, info, setLogLevel
+from mininet.util import dumpNodeConnections, quietRun, moveIntf
 from mininet.cli import CLI
-from mininet.node import OVSKernelSwitch
-from subprocess import Popen
-from time import sleep
+from mininet.node import Switch, OVSKernelSwitch
+
+from subprocess import Popen, PIPE, check_output
+from time import sleep, time
+from multiprocessing import Process
+from argparse import ArgumentParser
+
+import sys
 import os
 import termcolor as T
-from argparse import ArgumentParser
+import time
 
 setLogLevel('info')
 
@@ -20,18 +27,25 @@ args = parser.parse_args()
 FLAGS_rogue_as = args.rogue
 ROGUE_AS_NAME = 'S6'
 
+
 def log(s, col="green"):
     print(T.colored(s, col))
 
-class Router(OVSKernelSwitch):
+
+class Router(Switch):
+    """Defines a router in a network namespace with separate routing entries."""
+
     ID = 0
-    OVSVersion = "2.0.0"  # Added to bypass Mininet's OVS check
 
     def __init__(self, name, **kwargs):
         kwargs['inNamespace'] = True
-        OVSKernelSwitch.__init__(self, name, **kwargs)
+        super(Router, self).__init__(name, **kwargs)
         Router.ID += 1
         self.switch_id = Router.ID
+
+    @staticmethod
+    def setup():
+        return
 
     def start(self, controllers):
         pass
@@ -39,104 +53,115 @@ class Router(OVSKernelSwitch):
     def stop(self):
         self.deleteIntfs()
 
-    @staticmethod
-    def setup():
-        os.system("sysctl -w net.ipv4.ip_forward=1")  # Enable IP forwarding on the host
+    def log(self, s, col="magenta"):
+        print(T.colored(s, col))
 
-class Topology(Topo):  
+
+class GraphTopo(Topo):
+    """Graph topology for Autonomous Systems setup with routers and hosts."""
+
     def __init__(self):
-        super(Topology, self).__init__()
+        super(GraphTopo, self).__init__()
         num_hosts_per_as = 3
         num_ases = 6
-        routers = []
 
-        # Create routers
+        # Set up routers and hosts
+        routers = []
         for i in range(num_ases):
-            router = self.addSwitch('S%d' % (i + 1), cls=Router)
+            router = self.addSwitch(f'R{i+1}')
             routers.append(router)
 
-        # Create hosts and links within ASes
         for i in range(num_ases):
-            router = 'S%d' % (i + 1)
+            router = f'R{i+1}'
             for j in range(num_hosts_per_as):
-                hostname = 'h%d-%d' % (i + 1, j + 1)
-                host = self.addHost(hostname, ip="10.%d.%d.1/24" % (i + 1, j + 1), defaultRoute="via 10.%d.%d.254" % (i + 1, j + 1))
+                hostname = f'h{i+1}-{j+1}'
+                host = self.addHost(hostname)
                 self.addLink(router, host)
 
-        # Add links between ASes
-        for i in range(num_ases - 1):
-            self.addLink('S%d' % (i + 1), 'S%d' % (i + 2))
+        # Interconnect the AS routers
+        self.addLink('S2', 'S3')
+        self.addLink('S2', 'S4')
+        self.addLink('S2', 'S5')
+        self.addLink('S3', 'S4')
+        self.addLink('S3', 'S5')
+        self.addLink('S4', 'S5')
+        self.addLink('S1', 'S2')
+        self.addLink('S1', 'S3')
+        self.addLink('S5', 'S6')
 
-        # Add rogue AS
-        for j in range(num_hosts_per_as):
-            hostname = 'h6-%d' % (j + 1)
-            host = self.addHost(hostname, ip="20.0.%d.1/24" % (j + 1), defaultRoute="via 20.0.%d.254" % (j + 1))
-            self.addLink('S6', host)
 
-        self.addLink('S1', 'S6')  # Link rogue AS to AS1
+def getIP(hostname):
+    AS, idx = hostname.replace('h', '').split('-')
+    AS = int(AS)
+    if AS == 6:
+        AS = 1
+    ip = f'10.{10+AS}.0.{idx}/24'
+    return ip
+
+
+def getGateway(hostname):
+    AS, idx = hostname.replace('h', '').split('-')
+    AS = int(AS)
+    if AS == 6:
+        AS = 1
+    gw = f'10.{10+AS}.0.254'
+    return gw
+
 
 def start_router(router):
     """Function to start zebra and bgpd for BGP configurations."""
     zebra_cmd = f"/usr/lib/frr/zebra -f conf/zebra-{router.name}.conf -d -i /tmp/zebra-{router.name}.pid"
     bgpd_cmd = f"/usr/lib/frr/bgpd -f conf/bgpd-{router.name}.conf -d -i /tmp/bgp-{router.name}.pid"
-    
+
     router.cmd(zebra_cmd)
     router.waitOutput()
     router.cmd(bgpd_cmd)
     router.waitOutput()
-    router.cmd("ifconfig lo up")  # Enable loopback interface
+    router.cmd("ifconfig lo up")
     log(f"Started BGP and Zebra on {router.name}")
 
+
+def startWebserver(net, hostname, text="Default web server"):
+    host = net.getNodeByName(hostname)
+    return host.popen(f"python3 -m http.server 80 --bind 0.0.0.0 & echo '{text}'", shell=True)
+
+
 def main():
-    os.system("rm -f /tmp/S*.log /tmp/S*.pid logs/*")
+    os.system("rm -f /tmp/R*.log /tmp/R*.pid logs/*")
     os.system("mn -c >/dev/null 2>&1")
     os.system("killall -9 zebra bgpd > /dev/null 2>&1")
     os.system('pgrep -f webserver.py | xargs kill -9')
 
-    net = Mininet(topo=Topology(), switch=Router)
+    net = Mininet(topo=GraphTopo(), switch=Router)
     net.start()
 
-    # Enable IP forwarding on each router and add debug message
     for router in net.switches:
         router.cmd("sysctl -w net.ipv4.ip_forward=1")
-        output = router.cmd("sysctl net.ipv4.ip_forward")
-        log(f"IP forwarding status on {router.name}: {output.strip()}")
+        router.waitOutput()
 
-    log("Waiting %d seconds for sysctl changes to take effect..." % args.sleep)
+    log(f"Waiting {args.sleep} seconds for sysctl changes to take effect...")
     sleep(args.sleep)
 
-    # Start BGP and Zebra on each router
     for router in net.switches:
         if router.name == ROGUE_AS_NAME and not FLAGS_rogue_as:
             continue
-        
-        # Start zebra and bgpd
-        zebra_command = f"/usr/lib/frr/zebra -f conf/zebra-{router.name}.conf -d -i /tmp/zebra-{router.name}.pid"
-        bgpd_command = f"/usr/lib/frr/bgpd -f conf/bgpd-{router.name}.conf -d -i /tmp/bgp-{router.name}.pid"
-        
-        router.cmd(zebra_command)
-        router.cmd("sleep 1")  # Allow some time for zebra to start
-        router.cmd(bgpd_command)
-        router.cmd("ifconfig lo up")
-        
-        log(f"Started zebra and bgpd on {router.name}")
-        
-    # Configure hosts with IP and gateway
+        start_router(router)
+
     for host in net.hosts:
-        ip = "10.%d.%d.1/24" % (int(host.name.split('-')[0][1:]), int(host.name.split('-')[1]))
-        gw = "10.%d.%d.254" % (int(host.name.split('-')[0][1:]), int(host.name.split('-')[1]))
-        host.setIP(ip)
-        host.setDefaultRoute("via " + gw)
-        log(f"Configured host {host.name} with IP {ip} and gateway {gw}")
+        ip = getIP(host.name)
+        gw = getGateway(host.name)
+        host.cmd(f"ifconfig {host.name}-eth0 {ip}")
+        host.cmd(f"route add default gw {gw}")
 
     log("Starting web servers", 'yellow')
-    net.getNodeByName('h1-1').popen("python3 -m http.server 80", shell=True)
-    net.getNodeByName('h6-1').popen("python3 -m http.server 80", shell=True)  # Rogue web server
+    startWebserver(net, 'h1-1', "Default web server")
+    startWebserver(net, 'h6-1', "*** Attacker web server ***")
 
     CLI(net)
     net.stop()
     os.system("killall -9 zebra bgpd")
     os.system('pgrep -f webserver.py | xargs kill -9')
+
 
 if __name__ == "__main__":
     main()
--- connect.sh
+++ connect.sh
@@ -1,4 +1,16 @@
-router=${1:-S1}
+#!/bin/bash
+
+node=R5
+
+if [ "$#" -eq 1 ]; then
+    echo "Using node:"$1
+    node=$1
+fi
+
+echo "Connecting to node:"$node
+
+# Script to connect to a router's bgpd shell.
+router=${1:-$node}
 echo "Connecting to $router shell"
 
-sudo python3 run.py --node $router --cmd "telnet localhost bgpd"
+sudo python run.py --node $router --cmd "telnet localhost bgpd"
